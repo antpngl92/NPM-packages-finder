dff16c83ac00af6a943e4b02653ea5c5d4ac904a
Anton <nyagolov.anton@gmail.com>
2022-09-14 18:38:13 +0300

    Introduce `Library` page

---
+++ src/pages/Library/List/index.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+
+const LibraryList: FC = () => {
+  return (
+    <div>
+      <h1>Search for a Package</h1>
+      <div>RepositoriesList</div>
+    </div>
+  );
+};
+
+export default LibraryList;
