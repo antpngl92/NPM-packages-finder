1b3c57f323f1c7cc1c10882481d820d0452c858b
Anton <nyagolov.anton@gmail.com>
2022-09-14 19:19:54 +0300

    Add search functionality to `LibraryList` page

--- src/pages/Library/List/index.tsx
+++ src/pages/Library/List/index.tsx
@@ -1,10 +1,37 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
+
+import { useActions } from "../../../hooks/useActions";
+import { useTypedSelector } from "../../../hooks/useTypedSelector";
 
 const LibraryList: FC = () => {
+  const [term, setTerm] = useState("");
+  const { searchLibraries } = useActions();
+  const { data, error, loading } = useTypedSelector((state) => state.libraries);
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    searchLibraries(term);
+  };
+
   return (
     <div>
-      <h1>Search for a Package</h1>
-      <div>RepositoriesList</div>
+      <h1>Search for Library</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+        />
+        <button>Search</button>
+      </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading ...</h3>}
+      {!error &&
+        !loading &&
+        data.map((lib) => {
+          return <p>{lib}</p>;
+        })}
     </div>
   );
 };
