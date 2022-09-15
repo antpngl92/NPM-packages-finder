216d264c78ccf1229ffb9af47a1db001e5b9ab6f
Anton <nyagolov.anton@gmail.com>
2022-09-15 16:51:47 +0300

    Alter `App` and `index`

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import { Provider } from "react-redux";
-import { store } from "./redux";
+import { store } from "redux_state";
+import CssBaseline from "@mui/material/CssBaseline";
+import { Box } from "components";
+import theme from "theme";
 
 import { LibraryList } from "./pages";
 
 const App = () => {
   return (
     <Provider store={store}>
-      <LibraryList />
+      <CssBaseline />
+      <Box
+        sx={{
+          p: theme.spacing(5),
+          backgroundColor: theme.palette.background.default,
+          minHeight: "100vh",
+        }}
+      >
+        <LibraryList />
+      </Box>
     </Provider>
   );
 };
