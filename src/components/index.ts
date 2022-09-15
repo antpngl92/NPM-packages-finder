fbb8d96e2e033db0d3c68cf21d46f8451691d7dd
Anton <nyagolov.anton@gmail.com>
2022-09-15 16:52:14 +0300

    Introduce `Tooltip` and `Link` components

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,3 +6,5 @@ export { default as Typography } from "./Typography";
 export { default as Stack } from "./Stack";
 export { default as Grid } from "./Grid";
 export { default as Button } from "./Button";
+export { default as Tooltip } from "./Tooltip";
+export { default as Link } from "./Link";
